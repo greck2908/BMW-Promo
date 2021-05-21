62d9f64852e14018a43d56c544e2c1fcadf62426
Greck2908 <rallistab-82@bk.ru>
2021-05-21 16:24:48 +0600

    Update accordeon.js

--- js/accordeon.js
+++ js/accordeon.js
@@ -12,14 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     featureLinkElems.forEach((btn, index) => {
         btn.addEventListener('click', () => {
-            featureSubElems.forEach((featureSubElem) => {
+            if (btn.classList.contains('feature__link_active')) {
+                btn.classList.remove('feature__link_active');
+                featureSubElems[index].classList.add('hidden');
+            } 
+            else {
+                featureSubElems.forEach((featureSubElem) => {
                 featureSubElem.classList.add('hidden');
             });
-            featureLinkElems.forEach((featureLinkElem) => {
+                featureLinkElems.forEach((featureLinkElem) => {
                 featureLinkElem.classList.remove('feature__link_active');
             });
-            featureSubElems[index].classList.remove('hidden');
-            btn.classList.add('feature__link_active');
+                featureSubElems[index].classList.remove('hidden');
+                btn.classList.add('feature__link_active');
+            }
             
         });
     });
